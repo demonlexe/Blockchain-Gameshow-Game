ef48a28aed2c1eb443f74c1e8d808fbc67ec1f8f
demonlexe <alexiskaufman@gmail.com>
2024-12-24 22:18:53 -0600

    add carol's questions

--- client/src/mockdata/triviaQuestions.ts
+++ client/src/mockdata/triviaQuestions.ts
@@ -3,63 +3,241 @@ const questions: Array<{
   answer: string;
 }> = [
   {
-    question: "What fruit is red and often associated with teachers?",
-    answer: "Apple",
-  }, // A
-  { question: "What type of bear lives in the Arctic?", answer: "Bear" }, // B
-  // B 2
-  {
-    question: "What is the best city of NYC?",
-    answer: "Brooklyn",
+    question: "“Adventure of Sherlock Holmes” was written by which writer?",
+    answer: "Arthur Conan Doyle",
   },
   {
-    question: "What is a large feline known for its speed?",
-    answer: "Cheetah",
-  }, // C
-  { question: "What animal is known as man's best friend?", answer: "Dog" }, // D
+    question: "Which mountain range is the longest in the world?",
+    answer: "Andes",
+  },
   {
-    question: "What is the currency used in the European Union?",
-    answer: "Euro",
-  }, // E
+    question: "What vehicle is Volkswagen best known for in the world?",
+    answer: "Beetle",
+  },
+  { question: "Which musical legend is Jay-Z married to?", answer: "Beyonce" },
   {
-    question: "What do plants use to create energy through photosynthesis?",
-    answer: "Food",
-  }, // F
-  { question: "What is the largest mammal on Earth?", answer: "Giant Whale" }, // G
-  { question: "What is the capital of Japan?", answer: "Hiroshima" }, // H
-  { question: "What is the smallest prime number?", answer: "Integer Two" }, // I
-  { question: "What is the name for a baby kangaroo?", answer: "Joey" }, // J
-  { question: "What is the powerhouse of the cell?", answer: "Kinetosome" }, // K
-  { question: "What state of water exists at 100°C?", answer: "Liquid" }, // L
+    question: "According to the commercial, “what is the fabric of our lives”?",
+    answer: "Cotton",
+  },
   {
-    question: "Which country is famous for the Eiffel Tower?",
-    answer: "Monaco",
-  }, // M
-  { question: "What is the fastest land animal?", answer: "Nightingale" }, // N
-  { question: "What is the capital of Canada?", answer: "Ottawa" }, // O
-  { question: "Which ocean is the largest?", answer: "Pacific" }, // P
-  { question: "What is the term for a baby kangaroo?", answer: "Quokka" }, // Q
-  { question: "What is the chemical symbol for radium?", answer: "Radium" }, // R
-  { question: "Who painted the Mona Lisa?", answer: "Salvador Dalí" }, // S
+    question:
+      "What animal is known for its ability to change color to match its surroundings?",
+    answer: "Chameleon",
+  },
   {
-    question: "What bird is known for its colorful tail feathers?",
-    answer: "Turkey",
-  }, // T
-  { question: "What is the capital of Australia?", answer: "Uluru" }, // U
-  { question: "Which vitamin is produced by sunlight?", answer: "Vitamin D" }, // V
-  { question: "What is the largest desert on Earth?", answer: "Wadi" }, // W
+    question:
+      "Which popular soft drink was originally created as a medicine in the 19th century?",
+    answer: "Coca-Cola",
+  },
   {
-    question: "Which Greek letter represents an unknown in math?",
-    answer: "Xi",
-  }, // X
+    question:
+      "What fruit is known as the “king of fruits” and is banned in many hotels and public transportation in Southeast Asia due to its strong smell?",
+    answer: "Durian",
+  },
   {
-    question: "What gem is the hardest natural substance?",
-    answer: "Yellow Sapphire",
-  }, // Y
+    question:
+      "What prime time soap opera aired on CBS about the saga of the Ewing family and its massive oil empire based in a major city of Texas?",
+    answer: "Dallas",
+  },
   {
-    question: "What is the freezing point of water in Celsius?",
-    answer: "Zero",
-  }, // Z
+    question: "Which natural disaster is measured with a Richter scale?",
+    answer: "Earthquakes",
+  },
+  {
+    question:
+      "The smallest bone in the human body can be found in which body part?",
+    answer: "Ear",
+  },
+  {
+    question: "Who is the richest person in the world as of 2021?",
+    answer: "Elon Musk",
+  },
+  {
+    question:
+      "Who was the famous nurse known for her work during the Crimean War and is considered the founder of modern nursing?",
+    answer: "Florence Nightingale",
+  },
+  { question: "What is the largest bone in the human body?", answer: "Femur" },
+  { question: "Larry Page is the CEO of which company?", answer: "Google" },
+  { question: "What is the largest island in the world?", answer: "Greenland" },
+  {
+    question:
+      "Which fashion brand made the “Genius Jeans” that became part of the Guinness World Records?",
+    answer: "Gucci",
+  },
+  {
+    question: "Which animal can be seen on the Porsche logo?",
+    answer: "Horse",
+  },
+  { question: "What animal’s milk is pink?", answer: "Hippopotamus" },
+  {
+    question:
+      "Before becoming President, Barack Obama served as a U.S. Senator from which state?",
+    answer: "Illinois",
+  },
+  {
+    question:
+      "What is the title of the classic Greek epic attributed to Homer, telling the story of the Trojan War?",
+    answer: "Iliad",
+  },
+  {
+    question:
+      "A narrow strip of land with sea on either side, forming a link between two larger areas of land is known as an",
+    answer: "Isthmus",
+  },
+  {
+    question:
+      "Who won the Academy Award for Best Actor for his role in the 2020 film “Joker”?",
+    answer: "Joaquin Phoenix",
+  },
+  {
+    question: "Which country is known as the Land of the Rising Sun?",
+    answer: "Japan",
+  },
+  {
+    question:
+      "What vegetable used to be known as “Peasants Cabbage”, reflecting its status as a common food for the poor in many parts of Europe?",
+    answer: "Kale",
+  },
+  { question: "What was Superman’s birth name?", answer: "Kal-El" },
+  {
+    question: "Who did Forbes name the youngest self-made billionaire in 2019?",
+    answer: "Kylie Jenner",
+  },
+  {
+    question: "What fashion fad did the 1983 film “Flashdance” make popular?",
+    answer: "Legwarmers",
+  },
+  { question: "Who painted the Mona Lisa?", answer: "Leonardo da Vinci" },
+  {
+    question: "Who was the first woman to win a Nobel Prize (in 1903)?",
+    answer: "Marie Curie",
+  },
+  {
+    question:
+      "Which of these games includes the phrase “Do not pass go, do not collect $200”?",
+    answer: "Monopoly",
+  },
+  { question: "Who was the first man to moon walk?", answer: "Neil Armstrong" },
+  {
+    question:
+      "What is the world’s largest supplier of athletic shoes and apparel and is named after the Greek goddess of victory?",
+    answer: "Nike",
+  },
+  {
+    question: "What is the name of the home of the Greek Gods?",
+    answer: "Olympus",
+  },
+  { question: "Thor was the son of which God?", answer: "Odin" },
+  { question: "What is the state vegetable of Texas?", answer: "Onion" },
+  {
+    question:
+      "What classic video game requires you to eat all the dots throughout a maze?",
+    answer: "Pac-Man",
+  },
+  {
+    question: "Which name is rapper Sean Combs better known by?",
+    answer: "P. Diddy",
+  },
+  {
+    question:
+      "What word shares its meaning for putting out a fire and satisfying one’s thirst?",
+    answer: "Quench",
+  },
+  {
+    question:
+      "What writing tool preceded the invention of the metal-nibbed pen and is made from a moulted flight feather?",
+    answer: "Quill",
+  },
+  {
+    question:
+      "This social news and discussion website is known for their community building characterized by “r/”",
+    answer: "Reddit",
+  },
+  {
+    question: "What is the northernmost capital city in the world?",
+    answer: "Reykjavik, Iceland",
+  },
+  {
+    question: "Which watch company has a pointed crown as its logo?",
+    answer: "Rolex",
+  },
+  { question: "What do you call a group of fish?", answer: "School" },
+  {
+    question: "Which cartoon character lives in a pineapple under the sea?",
+    answer: "Spongebob Squarepants",
+  },
+  {
+    question: "What type of alcohol is traditionally used in a margarita?",
+    answer: "Tequila",
+  },
+  {
+    question: "What is the smallest size bed available in the United States?",
+    answer: "Twin",
+  },
+  {
+    question:
+      "What type of flowers are often associated with Holland and most commonly symbolize deep love?",
+    answer: "Tulips",
+  },
+  { question: "What is the official animal of Scotland?", answer: "Unicorn" },
+  {
+    question:
+      "This is a psychological phenomenon that describes the feeling of unease when confronted with something that is almost human but not quite",
+    answer: "Uncanny valley",
+  },
+  {
+    question: "What is the smallest country in the world?",
+    answer: "Vatican City",
+  },
+  { question: "What name does deer meat go by?", answer: "Venison" },
+  { question: "What are the bones in your spine called?", answer: "Vertebrae" },
+  { question: "What is the capital of New Zealand?", answer: "Wellington" },
+  {
+    question:
+      "On Sunday 18th June 1815, which famous battle took place and also shares a name with a brand of sparkling water?",
+    answer: "Waterloo",
+  },
+  {
+    question:
+      "What popular children’s puzzle book consisted of detailed pages of crowds where one must look for a red and white striped man?",
+    answer: "Where’s Waldo",
+  },
+  {
+    question:
+      "This is an example of an imaging procedure that uses radiation to produce pictures of the inside of the body",
+    answer: "X-Ray",
+  },
+  {
+    question:
+      "What Marvel Comics features a superhero team of mutants with superhuman abilities generally manifesting during puberty?",
+    answer: "X-men",
+  },
+  {
+    question:
+      "What food item is made when combining heated milk with bacteria in incubation at warm temperatures?",
+    answer: "Yogurt",
+  },
+  {
+    question:
+      "What was the name of the country in Central and Southeast Europe that translates to “Land of the South Slavs”?",
+    answer: "Yugoslavia",
+  },
+  {
+    question:
+      "What creature do most players first encounter in the popular kids game Minecraft?",
+    answer: "Zombies",
+  },
+  {
+    question:
+      "What 2016 Disney Animated buddy cop comedy film features Nick Wilde and Judy Hopps?",
+    answer: "Zootopia",
+  },
+  {
+    question:
+      "What country features the 355-foot Victoria Falls and is also known for their hyperinflation crisis?",
+    answer: "Zimbabwe",
+  },
 ];
 
 export default questions;
