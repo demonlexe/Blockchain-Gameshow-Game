86059b83329c86de514e656d2b18927d52b741fa
demonlexe <alexiskaufman@gmail.com>
2024-12-24 21:15:32 -0600

    add libraries

---
+++ client/src/app/host/page.tsx
@@ -0,0 +1,13 @@
+import { Container, Row, Col, Button } from "react-bootstrap";
+
+export default function HostPage() {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Button>Hey!</Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
