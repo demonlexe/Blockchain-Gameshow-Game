481e13afd4629abd2c9c70679b2730199df4fd81
demonlexe <alexiskaufman@gmail.com>
2025-01-22 12:20:02 -0600

    dynamic basepath calculation

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,26 @@
 import type { NextConfig } from "next";
 
+const ProdBasePath = "/Blockchain-Gameshow-Game";
+const isProd = process.env.NODE_ENV === "production";
+
+function getBasePath() {
+  let basePath = "";
+
+  if (isProd && ProdBasePath) {
+    basePath = ProdBasePath;
+  }
+
+  return basePath;
+}
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: "/Blockchain-Gameshow-Game",
+  assetPrefix: getBasePath(),
+
+  publicRuntimeConfig: {
+    basePath: getBasePath(),
+  },
 };
 
 export default nextConfig;
