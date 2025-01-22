98f9c4a377a747fd59a1df8f441d2bdd7b5365dc
demonlexe <alexiskaufman@gmail.com>
2025-01-22 12:35:16 -0600

    force usage of basePath

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import { Col, Container, Row } from "react-bootstrap";
+import config from "../../next.config";
+const basePath = config.basePath ?? "";
 
 export default function Home() {
   return (
     <Container style={{ marginTop: "1rem" }}>
       <Row>
         <Col>
-          <Link href={`/host`}>I am a host!</Link>
+          <Link href={`${basePath}/host`}>I am a host!</Link>
         </Col>
       </Row>
     </Container>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { usePathname, useRouter } from "next/navigation";
 import { Nav, Navbar } from "react-bootstrap";
+import config from "../../next.config";
+const basePath = config.basePath ?? "";
 
 export default function Header() {
   const router = useRouter();
@@ -17,7 +19,10 @@ export default function Header() {
         className="bg-dark navbar-dark"
       >
         {/* Left-most part: Logo and Title */}
-        <Navbar.Brand href={`/`} className="d-flex align-items-center">
+        <Navbar.Brand
+          href={`${basePath}/`}
+          className="d-flex align-items-center"
+        >
           <span
             className="text-white d-none d-md-block"
             style={{ fontSize: "1.2rem" }}
@@ -34,7 +39,7 @@ export default function Header() {
         >
           <Nav className="align-items-center">
             <Nav.Link
-              href={`/host`}
+              href={`${basePath}/host`}
               className="mx-2"
               style={{ color: getLinkColor("/host") }}
               onClick={() => router.push("/host")}
