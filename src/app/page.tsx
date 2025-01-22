3599c57f8ac5c06350aa7cab980abd0131d07fbb
demonlexe <alexiskaufman@gmail.com>
2025-01-22 12:22:01 -0600

    use runtime config in links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Link from "next/link";
 import { Col, Container, Row } from "react-bootstrap";
+import getConfig from "next/config";
+const { publicRuntimeConfig } = getConfig();
 
 export default function Home() {
   return (
     <Container style={{ marginTop: "1rem" }}>
       <Row>
         <Col>
-          <Link href="/host">I am a host!</Link>
+          <Link href={`${publicRuntimeConfig.basePath || ""}/host`}>
+            I am a host!
+          </Link>
         </Col>
       </Row>
     </Container>
